4a8d7e3473b8c62ccd33a601d190cdd9216721b6
gautamkolluru <gautamkolluru@192.168.0.18>
2019-05-27 22:00:46 -0700

    first change

---
+++ demo/src/main/java/com/example/demo/HelloWorldController.java
@@ -0,0 +1,22 @@
+package com.example.demo
+;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	
+	
+	@GetMapping(path="/login")
+	public String HelloWorld() {
+		
+		return "Helloworld";
+	}
+	
+	
+	
+}
